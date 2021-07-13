9b904287c05b5181c452da463bcd261e28389590
Ishita Gupta <65915970+ishitagupta014@users.noreply.github.com>
2021-07-13 22:06:45 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,35 @@
+
+
+const buttons = document.querySelectorAll('button');
+
+const display = document.querySelector('.display');
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', calculate);
+  });
+  function back() {
+    var val = display.value;
+      display.value = val.substr(0, val.length - 1);
+  }
+
+function calculate(event) {
+   
+    const clickedButtonValue = event.target.value;
+  
+    if (clickedButtonValue === '=') {
+     
+      if (display.value !== '') {
+       
+        display.value = eval(display.value);
+      }
+    } else if (clickedButtonValue === 'C') {
+      
+      display.value = '';
+    } else if(clickedButtonValue=='CE'){
+        back();
+    }
+    else {
+     
+      display.value += clickedButtonValue;
+    }
+}
